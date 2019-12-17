9cc48ecf8535cd3901163c2e0135146eb7f54227
KongBorup <avborup@gmail.com>
2019-12-17 18:47:54 +0100

    Add saving of FT results

--- src/main.rs
+++ src/main.rs
@@ -9,7 +9,48 @@ use csv;
 
 #[allow(non_snake_case)]
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    // Definér csv-filen, hvori data skal gemmes
+    // Dette gøres i sin egen blok, så al brugt hukommelse frigives, når dataet
+    // er gemt.
+    {
+        // Definér i hvilken fil, resultatet skal gemmes.
+        let csv_path = "/home/adrian/coding/projects/fourier-sop/data/plotdata.csv";
+        let mut wtr = csv::Writer::from_path(csv_path)?;
+
+        let fs = 1024.0;
+        // Generér inputsignalets samples som tidspunkter med komplekse
+        // funktionsværdier.
+        let (t, x) = generate_signal(fs);
+        // Benyt algoritmerne til at beregne Fouriertransformationen.
+        let X_fft = fourier_transform::fft(&x)?;
+        let X_dft = fourier_transform::dft(&x);
+
+        // Beregn hver X[k]-værdis tilhørende frekvens.
+        let n = x.len();
+        let T = (n as f64) / fs;
+        let f: Vec<f64> = (0..n)
+            .into_iter()
+            .map(|k| (k as f64) / T)
+            .collect();
+
+        // Normalisér X.
+        let X_fft: Vec<Complex<f64>> = X_fft.iter().map(|Xn| *Xn * (1.0 / (x.len() as f64))).collect();
+        let X_dft: Vec<Complex<f64>> = X_dft.iter().map(|Xn| *Xn * (1.0 / (x.len() as f64))).collect();
+
+        // Navngiv kolonnerne: tid, værdi, frekvens, FFT, DFT
+        wtr.write_record(&["t", "x", "f", "X_fft", "X_dft"])?;
+        // Gem dataet
+        for i in 0..t.len() {
+            wtr.write_record(&[
+                t[i].to_string(),
+                x[i].real().to_string(),
+                f[i].to_string(),
+                X_fft[i].abs().to_string(),
+                X_dft[i].abs().to_string(),
+            ])?;
+        }
+    }
+    
+    // Definér csv-filen, hvori benchmark-data skal gemmes
     let csv_path = "/home/adrian/coding/projects/fourier-sop/data/benchmark.csv";
     let mut wtr = csv::Writer::from_path(csv_path)?;
 
@@ -20,7 +61,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     for i in 0..12 {
         let fs = 2.0_f64.powf(i as f64);
         // Generér inputsignalets samples som komplekse tal
-        let x = generate_signal(fs);
+        let (_, x) = generate_signal(fs);
 
         // Benchmark DFT-algoritmen
         let avg_dur = utils::benchmark(|| { dft(&x); });
@@ -41,7 +82,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
 // Genererer et 4-sekunders signal af en sum af 2.5Hz- og 3Hz-bølger med `fs`
 // samples per sekund.
-pub fn generate_signal(fs: f64) -> Vec<Complex<f64>> {
+pub fn generate_signal(fs: f64) -> (Vec<f64>, Vec<Complex<f64>>) {
     let f_goal = (3.0, 2.5);
     let t_max = 4.0;
 
@@ -56,5 +97,5 @@ pub fn generate_signal(fs: f64) -> Vec<Complex<f64>> {
         .map(|val| Complex::new(val, 0.0))
         .collect();
 
-    x
+    (t, x)
 }
